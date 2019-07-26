71351fe5078686a59dd581716f84d28e586ae802
Julio Vedovatto <juliovedovatto@gmail.com>
2019-07-26 03:13:38 -0300

    Page Title support added, using vue-router

--- src/router.ts
+++ src/router.ts
@@ -6,22 +6,37 @@ import HomeView from './views/Home.vue';
 
 Vue.use(Router);
 
-export default new Router({
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      title: 'Vue.js Pokédex',
+    },
+  },
+  {
+    path: '/details/:number',
+    name: 'details',
+    meta: {
+      title: 'Pokémon Details - Vue.js Pokédex',
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "details" */ './views/Details.vue'),
+  },
+];
+
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/details/:number',
-      name: 'details',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "details" */ './views/Details.vue'),
-    },
-  ],
+  routes,
 });
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title;
+  next();
+});
+
+export default router;
